55f58f57fab41a5a88f84895c691c35972972b22
unknown <kaden.zipfel@hotmail.com>
2019-01-08 13:11:13 -0800

    Setup redux store

---
+++ client/src/containers/Navbar.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand">
+        <div className="container">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+              Forum
+            </Link>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li className="px-3">
+              <Link to="/new">New Post</Link>
+            </li>
+            <li className="px-3">
+              <Link to="/signup">Sign up</Link>
+            </li>
+            <li className="px-3">
+              <Link to="/signin">Log in</Link>
+            </li>
+            <li className="px-3">
+              <Link to="/logout">Log out</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
